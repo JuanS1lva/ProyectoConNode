97377f5e14899cd3e4373c57ed8f333c4582fd22
JuanS1lva <juan.silva.vane@gmail.com>
2021-02-04 21:36:24 -0500

    Primer commit con proyecto node

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ProyectoConNode
+Hola
---
+++ script.js
@@ -0,0 +1,27 @@
+// Importar moment
+var moment = require('moment'); // require
+
+let ahora = moment().format();   
+console.log("ahora: ",ahora);
+
+let febrero = moment("2020-02-29").isValid()
+
+
+
+console.log("febrero",febrero)
+
+let hoy = moment().format('YYYY'); 
+console.log("hoy",hoy);
+
+
+
+
+let febrero = "1997-08-01"
+// 1. Mostrar fecha mes(nombre del mes) dia año
+// 2. Mostar que dia fue el dia que naci: Jueves
+// 3. Mostrar hace cuantos años naciste
+// 4. Mostrar que fecha sera en 258 dias (dia-mes-año)
+// 5. Mostrar que fecha sera en 4 semanas (dia-mes-año)
+
+
+
